dd46baeaf2f6625f04086cd15ff55314f2be0905
NegativeRainbow <danishb@uw.edu>
2017-01-16 13:49:29 -0800

    Added screenshots and log

--- app/src/main/java/edu/washington/danishb/activityspy/MainActivity.java
+++ app/src/main/java/edu/washington/danishb/activityspy/MainActivity.java
@@ -2,12 +2,20 @@ package edu.washington.danishb.activityspy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate event fired");
+    }
+
+    @Override
+    protected void onDestroy(){
+        Log.e(TAG, "We're going down, Captain!");
+        super.onDestroy();
     }
 }
